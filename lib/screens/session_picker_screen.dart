e751651bfd70d7158dae99fb98cf8c002d06c50d
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-07-18 16:56:04 -0700

    added initial widgets to all screens

--- lib/screens/session_picker_screen.dart
+++ lib/screens/session_picker_screen.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class SessionPickerScreen extends StatelessWidget {
+  const SessionPickerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
