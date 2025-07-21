8189b02ecb8977a9ff979a1f5b8cf034016a3e91
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-07-21 15:27:14 -0700

    Converted screens to stateful widgets
    As a measure of "if we don't need it we can change it later", the stateless widgets for the screens are converted to stateful widgets

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CreateAccountScreen extends StatelessWidget {
+class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
 
+  @override
+  State<CreateAccountScreen> createState() => _CreateAccountScreenState();
+}
+
+class _CreateAccountScreenState extends State<CreateAccountScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
--- lib/screens/session_picker_screen.dart
+++ lib/screens/session_picker_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SessionPickerScreen extends StatelessWidget {
+class SessionPickerScreen extends StatefulWidget {
   const SessionPickerScreen({super.key});
 
+  @override
+  State<SessionPickerScreen> createState() => _SessionPickerScreenState();
+}
+
+class _SessionPickerScreenState extends State<SessionPickerScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
