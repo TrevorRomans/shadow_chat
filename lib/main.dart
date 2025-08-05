2b682a11a324725d2a39352d1bd3129747a5252e
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-08-05 14:39:44 -0700

    Tweaked the main file

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'screens/chat_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ShadowChat());
+  runApp(ShadowChat());
 }
 
 class ShadowChat extends StatelessWidget {
