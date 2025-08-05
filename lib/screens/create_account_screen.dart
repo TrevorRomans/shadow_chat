9cde757074fc89a6abf7869d2a304aaddbaa9a93
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-08-05 15:43:47 -0700

    Initial layout to Create Account screen

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shadow_chat/components/rounded_button.dart';
 import 'package:shadow_chat/constants.dart';
 import 'session_picker_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CreateAccountScreen extends StatefulWidget {
   static String id = 'create_account_screen';
@@ -13,11 +16,78 @@ class CreateAccountScreen extends StatefulWidget {
 
 // This is the first commit to the Account Creation Screen branch!
 class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  String email = '';
+  String password = '';
+  bool showSpinner = false;
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('This is the Account Creation Screen'),
+      backgroundColor: Colors.black45,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 200.0,
+                    child: Image.asset('images/chat_logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kInputTextDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kInputTextDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Create Account',
+                color: Colors.teal,
+                onPressed: () {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  // attempt to create user
+                  Navigator.pushNamed(context, SessionPickerScreen.id);
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: kIsFABEnabled
