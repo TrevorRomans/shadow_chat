4d37f52b198488ee04d98432d4222518f9d0b786
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-08-09 16:39:11 -0700

    Added user creation & error message

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -19,7 +19,15 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
   String email = '';
   String password = '';
   bool showSpinner = false;
+  bool isValid = true;
   final _auth = FirebaseAuth.instance;
+
+  bool looksLikeEmail(String email) {
+    // Pattern: 1+ characters, an @, 1+ characters, a . and 2+ characters
+    final pattern = r'^.+@.+\..{2,}$';
+    final valid = RegExp(pattern);
+    return valid.hasMatch(email);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,11 +66,17 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
               TextField(
                 keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
+                onTap: () {
+                  setState(() {
+                    isValid = true;
+                  });
+                },
                 onChanged: (value) {
                   email = value;
                 },
                 decoration: kInputTextDecoration.copyWith(
                   hintText: 'Enter your email',
+                  errorText: isValid ? null : 'Please enter a valid email',
                 ),
               ),
               SizedBox(
@@ -84,15 +98,42 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
               RoundedButton(
                 title: 'Create Account',
                 color: Colors.teal,
-                onPressed: () {
-                  setState(() {
-                    showSpinner = true;
-                  });
-                  //TODO: attempt to create user
-                  Navigator.pushNamed(context, SessionPickerScreen.id);
-                  setState(() {
-                    showSpinner = false;
-                  });
+                onPressed: () async {
+                  if (looksLikeEmail(email)) {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    //TODO: attempt to create user
+                    try {
+                      final newUser = _auth.createUserWithEmailAndPassword(
+                          email: email, password: password);
+                      Navigator.pushNamed(context, SessionPickerScreen.id);
+                    } on FirebaseAuthException catch (e) {
+                      showDialog<void>(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) => AlertDialog.adaptive(
+                          title: Text('Failed to Create Account'),
+                          content: Text(e.message!),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } else {
+                    setState(() {
+                      isValid = false;
+                    });
+                  }
                 },
               ),
             ],
