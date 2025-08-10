5cd2366fbd90aa145de69039b950ca1eeeafa4d8
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-08-10 14:32:55 -0700

    Added widget for message bubbles

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -52,3 +52,64 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble(
+      {super.key,
+      required this.sender,
+      required this.text,
+      required this.isMe});
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            userIsViewer ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.white,
+            ),
+          ),
+          Material(
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  )
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: userIsViewer ? Colors.black12 : kGold,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: !userIsViewer
+                      ? Colors.black54
+                      : isMe
+                          ? kGold
+                          : Colors.white60,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
