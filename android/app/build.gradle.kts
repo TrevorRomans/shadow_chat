84f65670527db04b111a3e80cd415a2eb0ff46dd
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-08-19 18:03:57 -0700

    Fixed Android Module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.shadow_chat"
-        minSdk = 23
+        minSdk { version = release(flutter.minSdkVersion) }
         targetSdk = 36
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
